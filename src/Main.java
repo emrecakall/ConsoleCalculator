23abebd06d7851a4cc6e39a80d3a63e7c73ac921
Laisya <cakalemre25@gmail.com>
2025-07-31 09:52:06 +0300

    feat: Add robust input validation for the first number

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Main {
 public static void main(String[] args){
 
     Scanner scanner = new Scanner(System.in);
+
+    System.out.println("-------------------------------");
+    System.out.println("Hesap Makinesine Hoş Geldiniz!");
+    System.out.println("-------------------------------");
+
+    double result = 0;
+    boolean isFirstNumberReceived = false;
+
+    while(!isFirstNumberReceived) {
+        System.out.print("\nLütfen bir sayı girin: ");
+        try {
+            result = scanner.nextDouble();
+            isFirstNumberReceived = true;
+        } catch (InputMismatchException e) {
+            System.out.println("Hatalı sayısal değer girdiniz!");
+            scanner.next();
+        }
+    }
 
 
 
