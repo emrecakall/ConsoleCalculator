0973fcea75a01c26c3d2eaf87feea70c5fe013a0
Laisya <cakalemre25@gmail.com>
2025-08-01 22:37:26 +0300

    feat: Create Calculator class with basic arithmetic methods

---
+++ src/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+
+        public double add(double number1, double number2){
+            return number1 + number2;
+        }
+
+        public double subtrack(double number1, double number2){
+        return number1 - number2;
+         }
+
+        public double multiply(double number1, double number2){
+        return number1 * number2;
+        }
+
+        public double divide(double number1, double number2){
+        return number1 / number2;
+        }
+
+         public double getRemainder(double number1, double number2){
+        return number1 % number2;
+        }
+}
